b9dc56446f5ea7e3861be533a5f126ff2684bf45
mjaral <mjaral@outlook.com>
2018-09-13 11:42:20 -0400

    Commit

--- Vistas/contactanos.php
+++ Vistas/contactanos.php
@@ -91,7 +91,30 @@
     </script>
     <!-- Barra de Menu  -->
     
-    <?php include_once('../Complementos/Menu.php'); ?>
+    <!-- Logo MisPerris-->
+    <div class="container-fluid" style="background-color: rgb(111, 198, 169)" align="center">
+        <a href="../Vistas/index.php"><img src="../img/Otras/logo.png" class="img.fluid" alt=""></a>
+    </div>
+    <!-- Barra de Menu  -->
+    <div class="contenedor" style="background-color: rgb(111, 198, 169)">
+        <ul class="nav justify-content-center" id="nav-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Vistas/index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Quienes Somos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Servicios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Vistas/contactanos.php">Contactanos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Procesos/Salir.php">Cerrar Session</a>
+            </li>
+        </ul>
+    </div>
 
     <form action="../Procesos/Registrar.php" method="POST" id="Formulario" autocomplete="off">
         <div id="login">
--- Vistas/index.php
+++ Vistas/index.php
@@ -25,7 +25,30 @@
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/lity.js"></script>
     
-    <?php include_once('../Complementos/Menu.php'); ?>
+    <!-- Logo MisPerris-->
+    <div class="container-fluid" style="background-color: rgb(111, 198, 169)" align="center">
+        <a href="../Vistas/index.php"><img src="../img/Otras/logo.png" class="img.fluid" alt=""></a>
+    </div>
+    <!-- Barra de Menu  -->
+    <div class="contenedor" style="background-color: rgb(111, 198, 169)">
+        <ul class="nav justify-content-center" id="nav-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Vistas/index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Quienes Somos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Servicios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Vistas/contactanos.php">Contactanos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="../Procesos/Salir.php">Cerrar Session</a>
+            </li>
+        </ul>
+    </div>
 
     <!-- Carrousel -->
     <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
